34a5baa3b4d8cb8a1e131784978a4c056ded11fa
Mario Rizki <mariorizki07@gmail.com>
2021-06-07 12:55:23 +0700

    add script for close collapsed hamburger menu when each link clicked

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
+// open menu when hamburger clicked
 const toggleButton = document.getElementById('hamburger');
 const navLinks = document.getElementById('nav-links');
 
 toggleButton.addEventListener('click', function() {
   navLinks.classList.toggle('active');
 });
+
+// close menu when one of nav link clicked
+
+const navLink = document.querySelectorAll('.nav-link');
+
+function linkAction() {
+  // When we click on each nav-link, we remove the active class
+  navLinks.classList.remove('active');
+}
+
+// navLink.forEach(function(n) {
+//   n.addEventListener('click', linkAction);
+// });
+navLink.forEach(n => n.addEventListener('click', linkAction));

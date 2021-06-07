68aa442f2164e26863cb50dbf5e8bef73455be6d
Mario Rizki <mariorizki07@gmail.com>
2021-06-07 23:51:33 +0700

    add close animation to navbar

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 // open menu when hamburger clicked
 const toggleButton = document.getElementById('hamburger');
-const navLinks = document.getElementById('nav-links');
+const navBar = document.getElementById('navbar');
 
 toggleButton.addEventListener('click', function() {
-  navLinks.classList.toggle('active');
+  navBar.classList.toggle('active');
+  toggleButton.classList.toggle('active');
 });
 
 // close menu when one of nav link clicked
@@ -11,11 +12,7 @@ toggleButton.addEventListener('click', function() {
 const navLink = document.querySelectorAll('.nav-link');
 
 function linkAction() {
-  // When we click on each nav-link, we remove the active class
-  navLinks.classList.remove('active');
+  navBar.classList.remove('active');
 }
 
-// navLink.forEach(function(n) {
-//   n.addEventListener('click', linkAction);
-// });
 navLink.forEach(n => n.addEventListener('click', linkAction));

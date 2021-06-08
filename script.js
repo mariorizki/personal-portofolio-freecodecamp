74b7155890c272b06e6603f017c8aebbb32bbb26
Mario Rizki <mariorizki07@gmail.com>
2021-06-09 04:58:17 +0700

    create typing animation and projects section

--- script.js
+++ script.js
@@ -16,3 +16,27 @@ function linkAction() {
 }
 
 navLink.forEach(n => n.addEventListener('click', linkAction));
+
+// typing animation
+const texts = ['a Frontend Developer', 'an UI designer', 'a Violinist'];
+let count = 0;
+let index = 0;
+let currentText = '';
+let letter = '';
+
+(function type() {
+  if (count === texts.length) {
+    count = 0;
+  }
+
+  currentText = texts[count];
+  letter = currentText.slice(0, ++index);
+
+  document.querySelector('.typing').textContent = letter;
+
+  if (letter.length === currentText.length) {
+    count++;
+    index = 0;
+  }
+  setTimeout(type, 200);
+})();
